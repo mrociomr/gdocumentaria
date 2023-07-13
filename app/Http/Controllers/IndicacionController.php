dcb044e068c12c01ba446c414009a105c028734b
Rocio <mimaquerar@est.unap.edu.pe>
2023-07-13 09:43:24 -0500

    migraciones

---
+++ app/Http/Controllers/IndicacionController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Indicacion;
+use Illuminate\Http\Request;
+
+class IndicacionController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Indicacion  $indicacion
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Indicacion $indicacion)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Indicacion  $indicacion
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Indicacion $indicacion)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Indicacion  $indicacion
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Indicacion $indicacion)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Indicacion  $indicacion
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Indicacion $indicacion)
+    {
+        //
+    }
+}
---
+++ app/Http/Controllers/MovimientoExternoController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\MovimientoExterno;
+use Illuminate\Http\Request;
+
+class MovimientoExternoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\MovimientoExterno  $movimientoExterno
+     * @return \Illuminate\Http\Response
+     */
+    public function show(MovimientoExterno $movimientoExterno)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\MovimientoExterno  $movimientoExterno
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(MovimientoExterno $movimientoExterno)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\MovimientoExterno  $movimientoExterno
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, MovimientoExterno $movimientoExterno)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\MovimientoExterno  $movimientoExterno
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(MovimientoExterno $movimientoExterno)
+    {
+        //
+    }
+}
