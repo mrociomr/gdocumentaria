18fad16537749e10b68dcf8c181a19c87f422da3
Cristiancg <45461497+armindnwyr@users.noreply.github.com>
2023-07-13 00:17:06 -0500

    creación de formulario

---
+++ resources/views/Formulario/edit.blade.php
@@ -0,0 +1,52 @@
+@extends('layouts.app', ['title' => __('User Profile')])
+
+@section('content')
+    @include('users.partials.header', [
+        'title' => __('Hello') . ' '. auth()->user()->name,
+    ])   
+
+    <div class="container-fluid mt--7">
+        <div class="row">
+            <div class="col-xl-12">
+                <div class="card bg-secondary shadow">
+                    <div class="card-header bg-white border-0">
+                        <div class="row align-items-center">
+                            <h3 class="mb-0">Registrar Formulario</h3>
+                        </div>
+                    </div>
+                    <div class="card-body">
+                        <form method="post" action="" autocomplete="off">
+                            @csrf
+                            @method('post')
+                            <h6 class="heading-small text-muted mb-4">Formulario</h6>
+                            <div class="pl-lg-4">
+                                <div class="form-group">
+                                    <label class="form-control-label">Name</label>
+                                    <input type="text" name="name" class="form-control form-control-alternative"  value="">
+                                </div>
+                                <div class="form-group">
+                                    <label class="form-control-label">Surname</label>
+                                    <input type="text" name="surname" class="form-control form-control-alternative"  value="">
+                                </div>
+                                <div class="form-group">
+                                    <label class="form-control-label">Lastname</label>
+                                    <input type="text" name="lastname" class="form-control form-control-alternative"  value="">
+                                </div>
+                                <div class="form-group">
+                                    <label class="form-control-label">Password</label>
+                                    <input type="password" name="password" class="form-control form-control-alternative"  value="">
+                                </div>
+
+                                <div class="text-center">
+                                    <a href="#" class="btn btn-danger mt-4">Cancel</a>
+                                    <button type="submit" class="btn btn-success mt-4">Save</button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+        @include('layouts.footers.auth')
+    </div>
+@endsection
