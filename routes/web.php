0c511b1504cf1005b23c1a0630095484b14a0641
Rocio <mimaquerar@est.unap.edu.pe>
2023-07-14 12:28:30 -0500

    correciones en rutas

--- routes/web.php
+++ routes/web.php
@@ -52,22 +52,7 @@ Route::group(['middleware' => 'auth'], function () {
 	Route::get('form', function(){
 		return view('Formulario.create');
 	});
-<<<<<<< HEAD
-    Route::resource('administrados', AdministradoController::class)->names('administrados');
-	Route::resource('areas', AreaController::class);
-	Route::resource('asignaciones', AsignacionController::class);
-	Route::resource('documentos', DocumentoController::class);
-	Route::resource('clases',ClaseController::class);
-	Route::resource('documentos-temp', DocumentoController::class);
-	Route::resource('indicaciones', DocumentoTempController::class);
-	Route::resource('movimientos', MovimientoController::class);
-	Route::resource('movimiento-externo', MovimientoExternoController::class);
-	Route::resource('movimiento-interno', MovimientoInternoController::class);
-	Route::resource('oficinas', OficinaController::class);
-	Route::resource('procedimientos', ProcedimientoController::class);
-	Route::resource('tipo-persona', TipoPersonaController::class);
-	Route::resource('tipo-documento', TipoDocumentoController::class);
-=======
+ 
 
 	Route::resource('administrados', AdministradoController::class)->names('administrados');
 	Route::resource('areas', AreaController::class)->names('areas');
@@ -83,7 +68,6 @@ Route::group(['middleware' => 'auth'], function () {
 	Route::resource('procedimientos', ProcedimientoController::class)->names('procedimientos');
 	Route::resource('tipo-persona', TipoPersonaController::class)->names('tipo-persona');
 	Route::resource('tipo-documento', TipoDocumentoController::class)->names('tipo-documento');	
->>>>>>> aae7c9612542645c014430c6ab0f5bb1398c6c3a
 });
 
 
