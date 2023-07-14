aae7c9612542645c014430c6ab0f5bb1398c6c3a
Rocio <mimaquerar@est.unap.edu.pe>
2023-07-14 12:08:29 -0500

    cambios en controladores y rutas

--- app/Http/Controllers/ProcedimientoController.php
+++ app/Http/Controllers/ProcedimientoController.php
@@ -1,10 +1,84 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Models\Procedimiento;
 use Illuminate\Http\Request;
 
 class ProcedimientoController extends Controller
 {
-    //
+                /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Procedimiento $procedimiento
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Procedimiento $procedimiento)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Procedimiento  $procedimiento
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Procedimiento $procedimiento)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Procedimiento  $procedimiento
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Procedimiento $procedimiento)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Procedimiento  $procedimiento
+     * @return \Illuminate\Http\Oficina
+     */
+    public function destroy(Procedimiento $procedimiento)
+    {
+        //
+    } 
 }
